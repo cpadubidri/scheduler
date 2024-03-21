7d009a042c6d2d8810160ec24895fca6577ff662
cpadubidri <chirag.padubidri@gmail.com>
2024-03-22 00:00:38 +0200

    Add calendar handler

---
+++ calHandler.py
@@ -0,0 +1,29 @@
+
+
+class CalendarHandler():
+
+    def __init__(self, email):
+        pass
+
+
+    def put_event(self):
+        pass
+
+    def get_events(self):
+        pass
+
+    def delete_event(self):
+        pass
+
+    def edit_event(self):
+        pass
+
+    
+
+
+
+
+if __name__=="__main__":
+    ch = CalendarHandler()
+
+
