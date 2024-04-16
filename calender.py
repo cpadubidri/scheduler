2d111b473e21d47d514630b30c708c00770a533f
jyotiharti <jyotiharti1@gmail.com>
2024-04-16 17:53:07 +0300

    Add calendar methods

--- calender.py
+++ calender.py
@@ -1,39 +1,36 @@
 from gcsa.google_calendar import GoogleCalendar
-from beautiful_date import Mar, hours
+from beautiful_date import Apr, hours
 from gcsa.event import Event
-import pandas as pd
+# import pandas as pd
 
-class GCalendar:
+class GCalendar():
 
     def __init__(self, email):
         self.gc = GoogleCalendar(email)
-        self.index=0
-        self.events = list(self.gc.get_events())
+        
 
+    def get_event(self, time_min=None, time_max=None):
+        event_list = list(self.gc.get_events(time_min, time_max))
+        return event_list
 
-    def put_event(self):
-        pass
-
-    def get_events(self):
-        return self.events
-    
-
-    def delete_event(self):
-        pass
-
-    def edit_event(self):
-        pass
-
-
-    
+    def add_event(self, event_name, start, end):
+       event = Event(event_name,start=start,end=end)
+       self.gc.add_event(event)
+        
 
 
 
 
 if __name__=="__main__":
-    ch = GCalendar(email='padubidrichirag0@gmail.com')
-    x = ch.get_events()
-    print(x)
+    calen = GCalendar(email='jyotiharti1@gmail.com')
+
+    # min_time = (17/Apr/2024)[10:00]
+    # max_time = (17/Apr/2024)[14:00]
+    # x = calen.get_event(min_time, max_time)
+    # print(x)
+
+    # calen.add_event()
+
 
 
 
